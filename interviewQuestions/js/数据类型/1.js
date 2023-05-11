2477cd9c36dfab6cbdc89b727ab87c48913aa447
liuyang <1773587692@qq.com>
2023-05-11 10:22:09 +0800

    update

--- interviewQuestions/js/数据类型/1.js
+++ interviewQuestions/js/数据类型/1.js
@@ -1,4 +1,20 @@
 console.log(typeof 1);
+console.log(typeof null); //object
 console.log(1 instanceof Number);
 console.log({} instanceof Object);
-console.log([] instanceof Object);
+console.log('--',[] instanceof Function);
+
+// instanceof 实现
+function myInstanceof(left, right) {
+  let proto = Object.getPrototypeOf(left);
+
+  let prototype = right.prototype;
+
+  while (true) {
+    if (!proto) false;
+    if (proto === prototype) return true;
+    proto = Object.getPrototypeOf(proto);
+  }
+}
+
+console.log(myInstanceof([], Object));
